8555e7a69b506c9bad5bab5c6a77684771e0babf
Zachary Smith <zach10smith@proton.me>
2025-07-16 14:31:12 -0500

    Added experimental tag identification

--- OTWArchiveHelper/OTWArchiveHelper.cs
+++ OTWArchiveHelper/OTWArchiveHelper.cs
@@ -1,5 +1,6 @@
 ﻿namespace OTWArchiveHelper; // change this line to your namespace/namespace location for manual installation
 using HtmlAgilityPack;
+using System.Numerics;
 using System.Text;
 
 public class OtwArchiveHelper
@@ -51,6 +52,63 @@ public class OtwArchiveHelper
         }
         HttpResponseMessage testResults = await _archiveClient.GetAsync(ArchivePath); // try to get the root page of the archive
         return testResults; // if we get a response, the connection is successful
+    }
+
+    private async Task<List<Dictionary<string,List<string>>>> GetTagBackend(string tag, int page)
+    {
+        if (string.IsNullOrEmpty(tag))
+        {
+            throw new ArgumentException("Tag name cannot be null or empty.");
+        }
+
+        string url = $"{ArchivePath}tags/{Uri.EscapeDataString(tag)}/works?page={page}"; // construct the URL for the fandom page
+        Task<HttpResponseMessage> tagPageDownload = _archiveClient.GetAsync(url);
+
+        HttpResponseMessage response = await tagPageDownload;
+
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Failed to retrieve tag page: {response.ReasonPhrase}");
+        }
+
+        var tagPage = new HtmlDocument();
+
+        tagPage.LoadHtml(await response.Content.ReadAsStringAsync());
+
+        var workListNode = tagPage.DocumentNode.SelectSingleNode("/html/body/div[@id='outer']/div[@id='inner']/div[@id='main']/ol[@class='work index group']"); 
+        var Result = new List<Dictionary<string,List<string>>>();
+
+        try
+        {
+            workListNode = tagPage.DocumentNode.SelectSingleNode("/html/body/div[@id='outer']/div[@id='inner']/div[@id='main']/ol[@class='work index group']"); // canon tag
+            foreach (var work in workListNode.ChildNodes)
+            {
+                var title = work.SelectSingleNode(".//div[@class='header module']/h4[@class='heading']/a").InnerText.Trim();
+                if (string.IsNullOrEmpty(title))
+                {
+                    throw new Exception("Work title is empty. This means its probably non-canonical.");
+                }
+                break; // we only need to check the first work to see if the tag is canonical or not
+            }
+            Result = await GetCanonTagPageBackend(tag, page); // get the canonical tag page
+        }
+        catch (Exception)
+        {
+            workListNode = tagPage.DocumentNode.SelectSingleNode("/html/body/div[@id='outer']/div[@id='inner']/div[@id='main']/div[@class='tag home profile']/div[@class='work listbox group']/ul[@class='index group']"); // non-canon tag
+            foreach (var work in workListNode.ChildNodes)
+            {
+                var title = work.SelectSingleNode(".//div[@class='header module'/h4[@class='heading']/a").InnerText.Trim();
+                if (string.IsNullOrEmpty(title))
+                {
+                    throw new Exception("This tag is neither canonical nor non-canonical.");
+                }
+                break;
+            }
+            Result = await GetNonCanonTagPageBackend(tag, page); // get the non-canonical tag page
+        }
+
+        return Result; // return the result list of dictionaries with work information
     }
     
     private async Task<List<Dictionary<string,List<string>>>> GetCanonTagPageBackend(string tag, int page) // async list of dictionaries with work information
@@ -171,7 +229,104 @@ public class OtwArchiveHelper
         
         return result;
     }
-    
+
+    private async Task<List<Dictionary<string, List<string>>>> GetNonCanonTagPageBackend(string tag, int page) // async list of dictionaries with work information
+    {
+        var result = new List<Dictionary<string, List<string>>>();
+
+        foreach (var work in workListNode.ChildNodes)
+        {
+            if (work.Name != "li") continue; // skip non-work nodes
+
+            Dictionary<string, List<string>> workList = new Dictionary<string, List<string>>();
+            // dictionary is Title, Author, WorkID, Fandoms, Warnings, Relationships, Characters, Tags, Summary, Word Count
+            // Extracting work details
+            List<string> title = new List<string>();
+            title.Add(work.SelectSingleNode(".//div[@class='header module']/h4[@class='heading']/a")?.InnerText.Trim() ?? "No Title Provided"); // works can have no title, so if the title tag isn't found, assume "No Title Provided"
+            List<string> authors = new List<string>();
+            foreach (var author in work.SelectNodes(".//div[@class='header module']/h4[@class='heading']/a[@rel='author']"))
+            {
+                authors.Add(author.InnerText.Trim());
+            }
+            List<string> workId = new List<string>();
+            workId.Add(work.SelectSingleNode(".//div[@class='header module']/h4[@class='heading']/a").GetAttributeValue("href", "/404").Split('/').Last()); // get the last part of the href, which is the work ID
+            List<string> fandoms = new List<string>();
+            foreach (var fandom in work.SelectNodes(".//div/h5/a[@class='tag']"))
+            {
+                fandoms.Add(fandom.InnerText.Trim());
+            }
+            List<string> warnings = new List<string>();
+            foreach (var warning in work.SelectNodes(".//ul[@class='tags commas']/li[@class='warnings']/strong/a[@class='tag']")) // works require a warning tag, so this should always return at least one
+            {
+                warnings.Add(warning.InnerText.Trim());
+            }
+            List<string> relationships = new List<string>();
+            var relationshipNodes = work.SelectNodes(".//ul[@class='tags commas']/li[@class='relationships']/a[@class='tag']");
+            if (relationshipNodes != null)
+            {
+                foreach (var relationship in relationshipNodes)
+                {
+                    relationships.Add(relationship.InnerText.Trim());
+                }
+            }
+            else
+            {
+                relationships.Add("No Relationship Tags"); // if no relationship tags are found, assume "No Relationship Tags"
+            }
+            List<string> characters = new List<string>();
+            var characterNodes = work.SelectNodes(".//ul[@class='tags commas']/li[@class='characters']/a[@class='tag']");
+            if (characterNodes != null)
+            {
+                foreach (var character in characterNodes)
+                {
+                    characters.Add(character.InnerText.Trim());
+                }
+            }
+            else
+            {
+                characters.Add("No Character Tags");
+            }
+
+            List<string> tags = new List<string>();
+            var tagNodes = work.SelectNodes(".//ul[@class='tags commas']/li[@class='freeforms']/a[@class='tag']");
+            if (tagNodes != null)
+            {
+                foreach (var tagNode in tagNodes)
+                {
+                    tags.Add(tagNode.InnerText.Trim()); // works can have no- yeah you get the idea by now
+                }
+            }
+            else
+            {
+                tags.Add("No Freeform Tags");
+            }
+
+            List<string> summary = new List<string>();
+            summary.Add(work.SelectSingleNode(".//blockquote[@class='userstuff summary']/p")?.InnerText.Trim() ?? "No Summary Provided"); // works can have no summary, so if the summary tag isn't found, assume "No Summary Provided"
+            List<string> wordCount = new List<string>();
+            wordCount.Add(work.SelectSingleNode(".//dl[@class='stats']/dd[@class='words']").InnerText.Trim()); // there always will be a word count.
+
+            // compiling the dictionary
+            workList = new Dictionary<string, List<string>>()
+            {
+                { "title", title },
+                { "authors", authors },
+                { "workId", workId },
+                { "fandoms", fandoms },
+                { "warnings", warnings },
+                { "relationships", relationships },
+                { "characters", characters },
+                { "tags", tags },
+                { "summary", summary },
+                { "wordCount", wordCount }
+            };
+
+            result.Add(workList); // add the work dictionary to the result list
+        }
+
+        return result;
+    }
+
     /// <summary>
     /// Gets the canonical tag page page as a list of dictionaries with List(string) key values. Good for additional processing/tag searches.
     /// </summary>
